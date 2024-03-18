f595a7646086c1bd3ddb7f774ea01f650cbef404
purewater02 <ksmin02@gmail.com>
2024-03-18 21:37:53 +0900

    init

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,95 @@
+package com.group.libraryapp.calculator
+
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import org.junit.jupiter.api.*
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertThrows
+import kotlin.math.exp
+
+class CalculatorTest {
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll() {
+            println("모든 테스트 시작 전")
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun afterAll() {
+            println("모든 테스트 종료 후")
+        }
+    }
+
+    @BeforeEach
+    fun beforeEach() {
+        println("각 테스트 시작 전")
+    }
+    @AfterEach
+    fun afterEach() {
+        println("각 테스트 종료 후")
+    }
+
+    @Test
+    fun add() {
+        // given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.add(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minus() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.minus(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiply() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.multiply(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divide() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.divide(3)
+
+        //then
+        assertThat(calculator.number).isEqualTo(1)
+    }
+
+    @Test
+    fun divideExceptionTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when & then
+        val message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.message
+
+        assertThat(message).isEqualTo("0으로 나눌 수 없습니다.")
+    }
+}
