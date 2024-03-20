f1d64f51e28b1adbea0cb8d215a4f1dedd8b7a26
purewater02 <ksmin02@gmail.com>
2024-03-20 16:53:02 +0900

    jackson for kotlin added

--- src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 class UserController(
     private val userService: UserService,
 ) {
